4e7c7ca566936bbb9384f5881ec42417e806eaf8
sparshkumar007 <sparshrajput92@gmail.com>
2023-07-23 22:01:58 +0530

    login and signup done

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import {connect} from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextRequest,NextResponse } from "next/server";
+import bcryptjs from 'bcryptjs';
+
+connect();
+
+export async function POST(request:NextRequest){
+    try {
+        const reqBody=await request.json();
+        const {username,email,password}=reqBody;
+        // check if user already exists
+        const user=await User.findOne({email});
+        console.log('hi');
+        if(user)
+        {
+            return NextResponse.json({message:"User already exists"},{status:400})
+        }
+        
+        // hashing of password with salt
+        const salt=await bcryptjs.genSalt(10);
+        const hashedPassword=await bcryptjs.hash(password,salt);
+        
+        const newUser =new User({
+            username,
+            email,
+            password:hashedPassword
+        });
+        
+        const savedUser=await newUser.save();
+        console.log(savedUser);
+        
+        return NextResponse.json({
+            message:"User saved successfully",
+            success:true,
+            savedUser
+        },{status:200});
+
+    } catch (error:any) {
+        return NextResponse.json({
+            message:"Internal Server Error",
+            error:error,
+    },{status:500});
+    }
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+// htmlFor making it a client interactive page
+"use client";
+
+export default function profilePage(){
+    return(
+        <div className="... flex flex-col min-h-screen items-center justify-center py-2">
+            profile page
+        </div>
+    )
+}
