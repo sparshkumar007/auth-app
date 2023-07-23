6cfebbc37e443d529635bac128476d01cf5eb42e
sparshkumar007 <sparshrajput92@gmail.com>
2023-07-23 22:07:31 +0530

    login and signup complete

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -60,7 +60,7 @@ export default function signupPage(){
         <input 
             className="g-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Enter your email" 
-            value={user.email}
+            value={user.email} 
             id="email"
             type="email"
             onChange={(e)=>{
